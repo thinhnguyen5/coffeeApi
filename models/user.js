703bed5d8a9198f130060c9a33b6bb80db2b8a1d
Thinh Duc <t9ngdu00@students.oamk.fi>
2022-01-04 03:52:02 +0200

    User model

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+//Create User Schema & model 
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        min: 6
+    },
+    name: { 
+        type: String, 
+        required: true,
+        min: 6
+    },
+    create_at: {
+        type: Date,
+        default: Date.now()
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+        max: 1024,
+        min: 6
+    },
+    role: {
+        type: String,
+        enum: ['basic', 'supervisor', 'admin'],
+        default: 'basic'
+    },
+    accessToken: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
